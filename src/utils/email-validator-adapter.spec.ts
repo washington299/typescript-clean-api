7c7093d7d6e8318fa2c6adddba20e504e44e183e
Washington Campos <washingtoncampos9@gmail.com>
2020-04-06 01:24:19 -0300

    test: ensure EmailValidatorAdapter calls validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -20,4 +20,11 @@ describe('EmailValidator Adapter', () => {
     const isValid = sut.isValid('valid_email@email.com');
     expect(isValid).toBe(true);
   });
+
+  test('Should call validator with correct email', () => {
+    const isEmailSpy = jest.spyOn(validator, 'isEmail');
+    const sut = new EmailValidatorAdapter();
+    sut.isValid('any_email@email.com');
+    expect(isEmailSpy).toBeCalledWith('any_email@email.com');
+  });
 });
