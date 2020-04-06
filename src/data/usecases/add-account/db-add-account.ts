9be4ba48b771813b4a3118517f3998336387cef4
Washington Campos <washingtoncampos9@gmail.com>
2020-04-06 20:55:58 -0300

    refactor: add try catch to DbAddAccount

--- src/data/usecases/add-account/db-add-account.ts
+++ src/data/usecases/add-account/db-add-account.ts
@@ -13,10 +13,14 @@ export class DbAddAccount implements AddAccount {
   }
 
   async add(accountData: AddAccountModel): Promise<AccountModel> {
-    const hashedPassword = await this.encrypter.encrypt(accountData.password);
-    await this.addAccountRepository.add(
-      Object.assign({}, accountData, { password: hashedPassword }),
-    );
-    return new Promise((resolve) => resolve(null));
+    try {
+      const hashedPassword = await this.encrypter.encrypt(accountData.password);
+      await this.addAccountRepository.add(
+        Object.assign({}, accountData, { password: hashedPassword }),
+      );
+      return new Promise((resolve) => resolve(null));
+    } catch (error) {
+      return new Promise((_resolve, reject) => reject(error));
+    }
   }
 }
