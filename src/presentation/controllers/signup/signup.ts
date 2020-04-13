f72dee2b5ac73d9066399598417b81556eea994c
Washington Campos <washingtoncampos9@gmail.com>
2020-04-13 17:53:55 -0300

    refactor: remove console from try catch

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -44,7 +44,6 @@ export class SignUpController implements Controller {
       });
       return ok(account);
     } catch (error) {
-      console.log(error);
       return serverError();
     }
   }
